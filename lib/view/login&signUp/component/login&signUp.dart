891ecdee953bfab2a8f027692496dc8522035b6c
Mohammadjavad Salehi <mjsalehi1381@gmail.com>
2023-04-20 00:58:48 -0700

    fix(loginsignUp): fix font and validation

--- lib/view/login&signUp/component/login&signUp.dart
+++ lib/view/login&signUp/component/login&signUp.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:FastFeed/utils/constants.dart';
 import 'package:FastFeed/view/verifyCode/components/verifyCode.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PhoneNumberDialog extends StatefulWidget {
   @override
@@ -8,66 +9,97 @@ class PhoneNumberDialog extends StatefulWidget {
 }
 
 class _PhoneNumberDialogState extends State<PhoneNumberDialog> {
-  // final TextEditingController _phoneController = TextEditingController();
-  String _phoneController="";
+  late String _phoneController = "";
   final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text("ورود یا عضویت"),
-      content: TextFormField(
-        decoration: InputDecoration(
-          suffixIcon: Icon(
-            Icons.phone,
-            color: Colors.black,
-          ),
-          border: OutlineInputBorder(),
-          labelStyle: TextStyle(color: Colors.black),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.black),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: YellowColor),
-          ),
-          labelText: "شماره تلفن همراه",
-          helperText: "",
-          helperStyle: TextStyle(
-            color: Colors.black,
+      contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+      title: Text(
+        "ورود یا عضویت",
+        style: TextStyle(fontFamily: "IranSansWeb"),
+      ),
+      content: SizedBox(
+        width: 500.w,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  suffixIcon: Icon(
+                    Icons.phone,
+                    color: Colors.black,
+                  ),
+                  border: OutlineInputBorder(),
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontFamily: "IranSansWeb",
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: YellowColor),
+                  ),
+                  labelText: "شماره تلفن همراه",
+                  helperText: "",
+                  hintText: "09123456789",
+                  helperStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: IranSansWeb,
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  _phoneController = value;
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty || value.length!=11) {
+                    return "لطفا شماره خود را وارد کنید";
+                  }
+                  return null;
+                },
+              ),
+            ],
           ),
         ),
-        style: TextStyle(
-          color: Colors.black,
-        ),
-        keyboardType: TextInputType.number,
-        onChanged: (value) {
-          _phoneController = value;
-        },
-        validator: (value) {
-          if (value == null || value.isEmpty || value.length < 1) {
-            return "لطفا شماره خود را وارد کنید";
-          }
-          return null;
-        },
       ),
       actions: [
         Center(
-          child: TextButton(
-            style: TextButton.styleFrom(
-                backgroundColor: YellowColor, fixedSize: Size.fromWidth(200)),
-            onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return ConfirmationDialog(phoneNumber: '$_phoneController');
-                  },
-                );
-
-            },
-            child: Text("ادامه",
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: YellowColor,
+                fixedSize: Size.fromWidth(200),
+              ),
+              onPressed: () {
+                if (_formKey.currentState?.validate() == true) {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return ConfirmationDialog(
+                        phoneNumber: _phoneController,
+                      );
+                    },
+                  );
+                }
+              },
+              child: Text(
+                "ادامه",
                 style: TextStyle(
                   fontSize: 15,
                   color: Colors.black,
-                )),
+                  fontFamily: "IranSansWeb",
+                ),
+              ),
+            ),
           ),
         ),
       ],
